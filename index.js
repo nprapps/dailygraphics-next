44dcca5790b9148f2795cb8cb40898bc45904bab
Thomas Wilburn <twilburn@npr.org>
2018-12-05 15:29:49 -0600

    Allow comments in JSON
    This is better for our config

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-var server = require("./server");
-var config = require("./config.json");
-server(config);
+(async function () {
+  var server = require("./server");
+  var readJSON = require("./lib/readJSON");
+
+  var config = await readJSON("./config.json");
+  server(config);
+})();
