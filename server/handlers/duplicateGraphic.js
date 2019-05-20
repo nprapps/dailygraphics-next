03894149e4df9b9db720c3efb8fbdd355d578f87
Thomas Wilburn <twilburn@npr.org>
2019-05-20 17:03:49 -0500

    Be more explicit about duplication method endpoint

--- server/handlers/duplicateGraphic.js
+++ server/handlers/duplicateGraphic.js
@@ -7,18 +7,21 @@ module.exports = async function(request, response) {
   var { original } = request.params;
   var { slug } = request.body;
 
+  console.log(`Attemptimg to duplicate ${original}...`);
+  console.log("Pausing live reload...");
   livereload.close();
 
   try {
     var duplicate = await duplicateGraphic(config, original, slug);
+    response.status(302);
+    response.set("Location", `/graphic/${duplicate}/`);
+    response.send();
   } catch (err) {
     response.status(500);
     console.log(err);
     response.send({ error: err.message });
   }
 
+  console.log("Restarting live reload...");
   livereload.reopen();
-  response.status(302);
-  response.set("Location", `/graphic/${duplicate}/`);
-  response.send();
 };
