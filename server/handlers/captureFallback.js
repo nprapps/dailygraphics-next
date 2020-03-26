b7eb5cffdc6fe5b156c8bf1323e063658c019d2a
thomaswilburn <twilburn@npr.org>
2020-03-26 14:14:16 -0500

    Add ability to specify fallbackPage in manifest

--- server/handlers/captureFallback.js
+++ server/handlers/captureFallback.js
@@ -1,12 +1,16 @@
 var path = require("path");
 var puppetry = require("../../lib/puppetry");
+var readJSON = require("../../lib/readJSON");
 
 module.exports = async function(request, response) {
   var app = request.app;
   var config = app.get("config");
   var port = app.get("port");
   var { slug } = request.params;
-  var url = `http://localhost:${port}/graphic/${slug}/index.html`;
+  var manifestPath = path.join(config.root, slug, "manifest.json");
+  var manifest = (await readJSON(manifestPath)) || {};
+  var page = manifest.fallbackPage || "index.html";
+  var url = `http://localhost:${port}/graphic/${slug}/${page}`;
   var destination = path.join(config.root, slug, "fallback.png");
 
   var puppet = puppetry(config);
