78cc96a62e897ad0d77fad77eab32064a25fa005
thomaswilburn <twilburn@npr.org>
2021-05-06 08:58:55 -0500

    Remove Google resolution code
    This edge case was more trouble than it was worth to handle

--- server/services/forceAuth.js
+++ server/services/forceAuth.js
@@ -1,20 +1,6 @@
 var { testConnection } = require("../../lib/sheetOps");
 var https = require("https");
 
-var resolveGoogle = function() {
-  return new Promise(function(ok, fail) {
-    var request = https.request({
-      url: "https://google.com",
-      method: "HEAD"
-    });
-    request.on("response", () => ok(true));
-    request.on("error", function(err) {
-      console.log(err);
-      ok(false);
-    });
-  });
-};
-
 var lastCheck = null;
 var checkInterval = 5 * 60 * 1000; // five minute pause on connection tests
 
@@ -34,18 +20,11 @@ module.exports = function(app) {
       next();
     } catch (err) {
       console.log(`Unable to authorize Google connection ("${err.message}")`);
-      // are we offline completely, or just unable to reach Google?
-      var reachable = await resolveGoogle();
-      if (reachable) {
-        // not connected, reroute to auth
-        console.log("Redirecting to authorization page");
-        response.status(302);
-        response.set("Location", "/authorize");
-        response.send();
-        return;
-      }
-      response.status(504);
-      response.send(`Could not establish network connection to Google servers.`);
+      // not connected, reroute to auth
+      console.log("Redirecting to authorization page");
+      response.status(302);
+      response.set("Location", "/authorize");
+      response.send();
     }
   };
 
